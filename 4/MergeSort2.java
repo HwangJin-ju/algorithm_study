14e237cc98c0ea14038f34227eeb0273db39569b
jj <nikeflyknitkr@gmail.com>
2024-02-04 16:33:29 +0900

    5-2 BFS까지

--- 4/MergeSort2.java
+++ 4/MergeSort2.java
@@ -1,8 +1,6 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
 public class MergeSort2 {
@@ -64,7 +62,7 @@ public class MergeSort2 {
         while (index1 <=m) {
             arr[k] = temp[index1];
             k++;
-            index1;
+            index1 ++;
         }
         while (index2 <=e ) {
             arr[k] = temp[index2];
--- 4/RadixSort.java
+++ 4/RadixSort.java
@@ -1,16 +1,25 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 public class RadixSort {
     public static int[] arr;
     public static void main(String[] args) throws NumberFormatException, IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int num = Integer.parseInt(br.readLine());
         arr = new int[num];
         for (int i = 0 ; i <= num ; i ++) {
             arr[i] = Integer.parseInt(br.readLine());
         }
         br.close();
+        executeRadixSort(arr, 5);
+        for (int i = 0 ; i < num ; i ++) {
+            bw.write(arr[i] + "\n");
+        }
+        bw.flush();
+        bw.close();
 
     }
     private static void executeRadixSort(int[] arr, int max_size) {
